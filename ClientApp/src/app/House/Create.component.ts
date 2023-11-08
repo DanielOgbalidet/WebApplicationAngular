8c2be8c0671525fda95b3447c634430c10e4288d
Deryja <90872205+Deryja@users.noreply.github.com>
2023-11-08 14:35:33 +0100

    fjernet create

--- ClientApp/src/app/House/Create.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-home',
-  templateUrl: './Create.component.html',
-  styleUrls: ['./Create.component.css']
-})
-export class HouseComponent {
-}
