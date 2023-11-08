447b47064947828098cea32a1d78b0960b43f9d7
DanielOgbalidet <Danielogbalidet@gmail.com>
2023-11-08 16:49:53 +0100

    la til service og house, ikke helt fikset kommer med feilmeldinger

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
+import { HousesComponent } from './houses/houses.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
-    HomeComponent
+    HomeComponent,
+    HousesComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -20,6 +22,7 @@ import { HomeComponent } from './home/home.component';
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'houses', component: HousesComponent },
       { path: '**', redirectTo: '', pathMatch: 'full'},
     ])
   ],
---
+++ ClientApp/src/app/houses/house.ts
@@ -0,0 +1,10 @@
+export interface IHouse {
+  HouseId: number;
+  Price: number;
+  Address: string;
+  ImageUrl: string;
+  Bedrooms: number;
+  Guests: number;
+  Description: string;
+  UserId: number;
+} 
