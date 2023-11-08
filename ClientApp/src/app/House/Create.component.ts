43b9713dba61eaeadc3370289c64a4ca81a9e8a2
Deryja <90872205+Deryja@users.noreply.github.com>
2023-11-08 14:27:09 +0100

    Satte inn create componentene

---
+++ ClientApp/src/app/House/Create.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './Create.component.html',
+  styleUrls: ['./Create.component.css']
+})
+export class HouseComponent {
+}
